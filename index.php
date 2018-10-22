4daf29a2bcc485e72984b42b931fce97b71290f0
José Joaquín Garci <josejoaquin.garci@iesdonana.org>
2018-10-22 18:02:41 +0200

    Refactorización

--- index.php
+++ index.php
@@ -6,11 +6,35 @@
     </head>
     <body>
         <?php
+        const OP = ['+','-','*','/'];
+
+        function selected ($op1, $op2)
+        {
+            return $op1 == $op2 ? "selected" : "";
+        }
+
         $op1 = isset($_GET['op1']) ? trim($_GET['op1']) : '0';
         $op2 = isset($_GET['op2']) ? trim($_GET['op2']) : '0';
-        $oper = isset($_GET['oper']) ? trim($_GET['oper']) : '+';
+        $op = isset($_GET['op']) ? trim($_GET['op']) : '+';
         $res = '';
-        switch($oper) {
+        ?>
+        <form action="" method="get">
+            <label for="op1">Primer operando:</label>
+            <input id="op1" type="text" name="op1" value="<?= $op1 ?>"><br/>
+            <label for="op2">Segundo operando:</label>
+            <input id="op2" type="text" name="op2" value="<?= $op2 ?>"><br/>
+            <label for="op">Operador:</label>
+            <select name="op">
+                <?php foreach ($OP as $o): ?>
+                    <option value="<?= $o ?>" <?= selected($op, "o") ?> >
+                        <?= $o ?>
+                    </option>
+                <?php endforeach ?>
+            </select><br/>
+            <input type="submit" value="Calcular">
+        </form>
+        <?php
+        switch($op) {
             case '+':
                 $res = $op1+$op2;
                 break;
@@ -25,18 +49,6 @@
                 break;
         }
         ?>
-        <form action="" method="get">
-            <label for="op1">Primer operando:</label>
-            <input id="op1" type="text" name="op1" value="<?= $op1 ?>">
-            <br>
-            <label for="op2">Segundo operando:</label>
-            <input id="op2" type="text" name="op2" value="<?= $op2 ?>">
-            <br>
-            <label for="oper">Operador:</label>
-            <input id="oper" type="text" name="oper" value="<?= $oper ?>">
-            <br>
-            <input type="submit" value="Calcular">
-        </form>
         <h3>Resultado: <?= $res ?></h3>
     </body>
 </html>
