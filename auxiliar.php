e62a6ef9e39777142e41b089bb4896fceb219efa
José Joaquín Garci <josejoaquin.garci@iesdonana.org>
2018-10-24 20:25:26 +0200

    Versión sólo con código PHP en principal

--- auxiliar.php
+++ auxiliar.php
@@ -1,6 +1,52 @@
-<?php function formulario($op1, $op2, $op, $ops)
+<?php
+
+function muestraErrores($error)
 {
-?>
+    foreach ($error as $err): ?>
+        <h3>Error: <?= $err ?></h3>
+    <?php endforeach;
+}
+
+function mostrarResultado($op1, $op2, $op)
+{ ?>
+    <h3>Resultado: <?= calcula($op1, $op2, $op) ?></h3>
+<?php
+}
+
+function compruebaParametros($par, &$error)
+{
+    if (empty($_GET)) {
+        if (empty(array_diff_key($_GET, $par)) &&
+            empty(array_diff_key($par, $_GET))) {
+            return array_map('trim', $_GET);
+        } else {
+            $error[] = "Los parámetros recibidos no son los correctos.";
+        }
+    }
+    return $par;
+}
+
+function compruebaValores($op1, $op2, $op, $ops, &$error)
+{
+    if (!is_numeric($op1)) {
+        $error[] = "El primer operando no es un número.";
+    }
+    if (!is_numeric($op2)) {
+        $error[] = "El segundo operando no es un número.";
+    }
+    if (!in_array($op, $ops)) {
+        $error[] = "El operador no es válido.";
+    }
+}
+
+function selected ($op1, $op2)
+{
+    return $op1 == $op2 ? "selected" : "";
+}
+
+function formulario($op1, $op2, $op, $ops)
+{
+    ?>
     <form action="" method="get">
         <label for="op1">Primer operando:</label>
         <input id="op1" type="text" name="op1" value="<?= $op1 ?>"><br/>
@@ -16,12 +62,7 @@
         </select><br/>
         <input type="submit" value="Calcular">
     </form>
-<?php
-}
-
-function selected ($op1, $op2)
-{
-    return $op1 == $op2 ? "selected" : "";
+    <?php
 }
 
 function calcula($op1, $op2, $op) {
--- index.php
+++ index.php
@@ -11,41 +11,22 @@
         const OP = ['+','-','*','/'];
         const PAR = ['op' => '+', 'op1' => '0', 'op2' => '0'];
 
-        $op1 = $op2 = $op = null;
         $error = [];
 
-        // Comprobación de parámetros:
-
-        if (empty($_GET)) {
-            extract(PAR);
-        } elseif (empty(array_diff_key($_GET, PAR)) &&
-                  empty(array_diff_key(PAR, $_GET))) {
-            extract(array_map('trim', $_GET), EXTR_IF_EXISTS);
-        } else {
-            $error[] = "Los parámetros recibidos no son los correctos.";
-        }
+        // Comprobación de parámetros
+        extract(compruebaParametros(PAR, $error));
 
         if (empty($error)){
             // Comprobación de valores:
-            if (!is_numeric($op1)) {
-                $error[] = "El primer operando no es un número.";
-            }
-            if (!is_numeric($op2)) {
-                $error[] = "El segundo operando no es un número.";
-            }
-            if (!in_array($op, OP)) {
-                $error[] = "El operador no es válido.";
-            }
+            compruebaValores($op1, $op2, $op, OP, $error);
         }
-        
+
         formulario($op1, $op2, $op, OP);
 
-        if (empty($error)): ?>
-            <h3>Resultado: <?= calcula($op1, $op2, $op)?></h3>
-        <?php else: ?>
-            <?php foreach ($error as $err): ?>
-                <h3>Error: <?= $err ?></h3>
-            <?php endforeach;
-        endif; ?>
+        if (empty($error)) {
+            mostrarResultado($op1, $op2, $op);
+        } else {
+            muestraErrores($error);
+        } ?>
     </body>
 </html>
